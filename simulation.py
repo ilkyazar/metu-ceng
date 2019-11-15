8c15570051b46568de1c1175b731b6e9ce5c6476
ILKYAZ ARABACI <e2236933@cenginek>
2019-11-15 15:24:07 +0300

    add a segment to simulation

--- simulation.py
+++ simulation.py
@@ -13,11 +13,20 @@ options = DrawOptions()
 space = pymunk.Space()
 space.gravity = 0, -100
 
-ball1 = BowlingBall(center = (400,400))
+ball1 = BowlingBall(center = (400,600))
 ball2 = TennisBall(center = (600,600))
 ball3 = MarbleBall(center = (500, 600))
 
 space.add(ball1, ball1.body, ball2, ball2.body, ball3, ball3.body)
+
+mass = 1
+# (mass, a, b, radius)
+segment_moment = pymunk.moment_for_segment(mass, (600,300), (600,700), 2)
+segment_body = pymunk.Body(mass, segment_moment)
+segment_shape = pymunk.Segment(segment_body, (0,0), (0,400), 2)
+segment_body.position = 600, 300
+
+space.add(segment_body, segment_shape)
 
 @window.event
 def on_draw():
