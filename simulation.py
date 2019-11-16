b42342b10f94eff0975d873bef93b9797231d7ef
ILKYAZ ARABACI <e2236933@cenginek>
2019-11-16 13:52:18 +0300

    add rotating segment to space

--- simulation.py
+++ simulation.py
@@ -15,23 +15,23 @@ from pymunk.pyglet_util import DrawOptions
 window = pyglet.window.Window(1280, 720, "Pymunk Tester", resizable=False)
 options = DrawOptions()
 space = pymunk.Space()
-space.gravity = 0, -100
+space.gravity = 0, -50
 
 
 #####
 
-ball1 = BowlingBall(center = (400,600))
+ball1 = BowlingBall(center = (250, 500))
 #ball1.getMass()
-ball2 = TennisBall(center = (600,600))
-ball3 = MarbleBall(center = (500, 600))
+ball2 = TennisBall(center = (70,60))
+ball3 = MarbleBall(center = (90, 60))
 
 space.add(ball1, ball1.body, ball2, ball2.body, ball3, ball3.body)
 
 
 #####
 
-block1 = BookBlock(center = (800, 800))
-block2 = DominoBlock(center = (900, 800))
+block1 = BookBlock(center = (100, 200))
+block2 = DominoBlock(center = (200, 200))
 
 space.add(block1, block1.body, block2, block2.body)
 
@@ -44,6 +44,17 @@ segment = Segment(mass, (600,300), (600,700), 2)
 space.add(segment, segment.body)
 
 
+#####
+rotation_center_body = pymunk.Body(body_type = pymunk.Body.STATIC) # 1
+rotation_center_body.position = (300, 300)
+
+body = pymunk.Body(10, 10000) # 2
+body.position = (300, 300)
+l1 = pymunk.Segment(body, (-150, 0), (150.0, 0.0), 5.0)
+
+rotation_center_joint = pymunk.PinJoint(body, rotation_center_body, (0,0), (0,0)) # 3
+
+space.add(l1, body, rotation_center_joint)
 #####
 
 
