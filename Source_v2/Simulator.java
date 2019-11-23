f56cfd08798642e70faf29f86a2aca22b9e98e94
ilkyazar <ilkyaz.arabaci@gmail.com>
2019-11-23 18:44:48 +0300

    update hw1

--- Source_v2/Simulator.java
+++ Source_v2/Simulator.java
@@ -2,8 +2,8 @@ public class Simulator {
 
   public static void main(String[] args) {
     SessionParser parser = new SessionParser();
-    //Session s = parser.parse("monza.xml");
-    Session s = parser.parse("spa.xml");
+    Session s = parser.parse("monza.xml");
+    //Session s = parser.parse("spa.xml");
     s.simulate();
   }
 }
