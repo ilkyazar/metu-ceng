5d284c681d7f680126c588128f80c5b4c07e9e7b
ilkyazar <ilkyaz.arabaci@gmail.com>
2020-03-07 20:47:21 +0300

    erör

--- server.cpp
+++ server.cpp
@@ -5,6 +5,7 @@
 #include <sys/wait.h>
 #include <sys/time.h>
 #include <unistd.h>
+#include <vector>
 #include "./lib/logging.h"
 #define PIPE(fd) socketpair(AF_UNIX, SOCK_STREAM, PF_UNIX, fd)
 
@@ -12,7 +13,8 @@ int starting_bid;
 int minimum_increment;
 int number_of_bidders;
 
-std::multimap<std::string, int> info_of_bidders;
+std::multimap<std::string, int> info_of_bidders; // Executable and delay
+std::multimap<std::string, std::vector<std::string>> bidder_lines; // Executable and other arguments
 
 void readBidderInfo() {
   std::string bidder_executable;
@@ -31,8 +33,26 @@ void readBidderInfo() {
   info_of_bidders.insert(std::pair <std::string, int> (bidder_executable, delay)); 
 }
 
+void readBidderLines() {
+  std::string bidder_executable;
+  int number_of_arguments;
+
+  std::cin >> bidder_executable >> number_of_arguments;
+
+  std::vector<std::string> args;
+
+  int arg_count = 0;
+  while (arg_count < number_of_arguments) {
+    int arg;
+    std::cin >> arg;
+    args[arg_count] = arg;
+    arg_count++;
+  }
+
+  bidder_lines.insert(std::pair <std::string, std::vector<std::string>> (bidder_executable, args)); 
+}
+
 void server(int fd[]) {
-  std::cout << "SERVER" << std::endl;
 
   int open = 1; // keep a flag for if pipe is still open
   fd_set readset;
@@ -42,48 +62,49 @@ void server(int fd[]) {
   ii* data;
 
   while (open) {
-    std::cout << "WHILE" << std::endl;
     // initialize a blocking set for select()
     FD_ZERO(&readset);
     FD_SET(fd[0], &readset);
 
     // Wait up to five seconds
     struct timeval tv;
-    tv.tv_sec = 3;
+    tv.tv_sec = 5;
     tv.tv_usec = 0;
     
     // block until any of the bidders have data to read
     int retval = select(m, &readset, NULL, NULL, &tv);  // no wset, eset, timeout
+
+    std::cout << retval << std::endl;
+    /*
     if (retval == -1) {
       perror("select()");
+      std::cout << "Select returned -1." << std::endl;
     }
+    */
     
     // now readset contains the fd's with available data
     // check if the pipe is in readset
 
-    else if (FD_ISSET(fd[0], &readset)) { 
-      std::cout << "else if" << std::endl;
+    if (FD_ISSET(fd[0], &readset)) { 
       
       r = read(fd[0], data, 50);
-      std::cout << "r = " << r << std::endl;
       if (r == -1) {
-        std::cout << "En error occured while reading from the file descriptor." << std::endl;
+        std::cout << "An error occured while reading from the file descriptor." << std::endl;
+        std::cout << data->type << std::endl;
         open = 0;
       }
 			else if (r == 0) { // EOF
 				open = 0;
-        std::cout << "if" << std::endl;
       }
 			else {
         //std::cout << data->type << std::endl;
         print_input(data, 0);
-        std::cout << "else" << std::endl;
       }
       
     }
 
     else
-      std::cout << "No data within three seconds." << std::endl;
+      std::cout << "No data within 5 seconds." << std::endl;
 
   }
 
@@ -96,29 +117,22 @@ int main(int argc, char** argv) {
   int n = 0;
   while (n < number_of_bidders) {
     readBidderInfo();
+    //readBidderLines(); //key: executable value: args
     n++;
   }
 
-  for (auto it = info_of_bidders.cbegin(); it != info_of_bidders.cend(); ++it) {
-    std::cout << it->first << " " << it->second << std::endl;
-  }
 
   int i = 0;
   while (i < number_of_bidders) {
     auto it = info_of_bidders.cbegin();
+    //auto it = bidder_lines.cbegin();
 
     int fd[2];
-    // support communication via bidirectional sockets
     PIPE(fd);
 
     int p;
     int w;
-    /*
-      fd[0] --> read
-      stdin: 0
-      stdout: 1
-      stderr: 2
-    */
+
 
     if (p = fork()) { // PID of the child process is returned in the parent
 		  close(fd[1]);
@@ -130,12 +144,17 @@ int main(int argc, char** argv) {
     }
     else { // 0  is returned in the child
       close(fd[0]);
-      //dup2(fd[1], 1);
-      //std::cout << "DUP2 FD 1 -> 1" << std::endl;
-      //dup2(fd[1], 0);
-      //std::cout << "DUP2 FD 1 -> 0" << std::endl;
+      dup2(fd[1], 1);
+      dup2(fd[1], 0);
       close(fd[1]);
-      execl((it->first).c_str(), (char *) 0);
+      char arr[] = {"2000"};
+      char* args[1];
+      args[0] = "2000";
+      //execl(("./bin/" + it->first + "2000").c_str(), arr, (char *) 0);
+      //execl(("./bin/" + it->first).c_str(), "2000", NULL);
+
+      //execvp(("./bin/" + it->first).c_str(), NULL); // ---> delay should be given message
+      execvp(("./bin/" + it->first).c_str(), args);
     }
   
     i++;
