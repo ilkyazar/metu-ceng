4e0c0eb03900bb23d267afe4080abfa35e8c194b
ilkyazar <ilkyaz.arabaci@gmail.com>
2020-03-09 15:37:14 +0300

    args

--- server.cpp
+++ server.cpp
@@ -14,35 +14,49 @@ int starting_bid;
 int minimum_increment;
 int number_of_bidders;
 
-std::multimap<std::string, std::vector<std::string>> bidder_lines; // Executable and other arguments
-std::multimap<std::string, int> bidder_arg_counts; // Executable and argument count
+//std::multimap<const char*, char**> bidder_lines; // Executable and other arguments
+std::multimap<const char*, std::vector<std::string>> bidder_lines; // Executable and other arguments
+std::multimap<const char*, int> bidder_arg_counts; // Executable and argument count
 
 void readBidderLines() {
-  std::string bidder_executable;
+  const char* bidder_executable;
   int number_of_arguments;
 
-  std::cin >> bidder_executable >> number_of_arguments;
+  std::string bidder_exec_str;
 
-  std::cout << "Bidder Executable is: " << bidder_executable;
+  std::cin >> bidder_exec_str >> number_of_arguments;
+
+  bidder_executable = bidder_exec_str.c_str();
+
+  std::cout << "Bidder Executable is: ";
+  for (int i=0; bidder_executable[i]; i++) {
+    std::cout << bidder_executable[i];
+  }
   std::cout << " | Number of Arguments: " << number_of_arguments;
 
+  //char* args[number_of_arguments];
   std::vector<std::string> args;
+
 
   int arg_count = 0;
   while (arg_count < number_of_arguments) {
-    std::string arg;
-    std::cin >> arg;
+    //const char* arg;
+    std::string arg_str;
+    std::cin >> arg_str;
 
-    args.push_back(arg);
+    //arg = arg_str.c_str();
 
-    std::cout << " | Argument " << arg_count << ": " << arg;
+    //args[arg_count] = arg;
+    args.push_back(arg_str);
+
+    std::cout << " | Argument " << arg_count << ": " << arg_str;
 
     arg_count++;
   }
   std::cout << std::endl;
 
-  bidder_lines.insert(std::pair <std::string, std::vector<std::string>> (bidder_executable, args));
-  bidder_arg_counts.insert(std::pair <std::string, int> (bidder_executable, number_of_arguments)); 
+  bidder_lines.insert(std::pair <const char*, std::vector<std::string>> (bidder_executable, args));
+  bidder_arg_counts.insert(std::pair <const char*, int> (bidder_executable, number_of_arguments)); 
 
 }
 
@@ -106,7 +120,7 @@ void server(int fd[]) {
 
 }
 
-int main(int argc, char** argv) {
+int main() {
 
   std::cin >> starting_bid >> minimum_increment >> number_of_bidders;
 
@@ -119,30 +133,34 @@ int main(int argc, char** argv) {
   }
 
   int fds[number_of_bidders][2];
+  pid_t pids[number_of_bidders];
 
-  char* args[1];
-  args[0] = "2000";
+  char* argument = "2000";
+
+  char* const argv[2] = {argument, NULL};
 
   for (int i = 0; i < number_of_bidders; i++) {
-    std::multimap<std::string, std::vector<std::string>>::iterator it;
+    std::multimap<const char*, std::vector<std::string>>::iterator it;
     it = bidder_lines.begin();
 
-    std::multimap<std::string, int>::iterator c;
+    std::multimap<const char*, int>::iterator c;
     c = bidder_arg_counts.begin();
 
     PIPE(fds[i]);
 
-    int p = fork();
+    pids[i] = fork();
     
-    if (p == 0) {
+    if (pids[i] == 0) {
       dup2(fds[i][0], 1);
       dup2(fds[i][0], 0);
       close(fds[i][1]);
-      execv("./bin/Bidder", args);
+      //fflush(stdout);
+      //dup2(fds[i][1], 0);
+      execv("./Bidder", argv);
     }
-    else {
-      //close(fds[i][0]);
-    }
+    
+    close(fds[i][0]);
+    
 
     it++;
   }
@@ -157,8 +175,8 @@ int main(int argc, char** argv) {
 
   int m = 0;
   for (int i = 0; i < number_of_bidders; i++) {
-    if (fds[i][0] > m) {
-      m = fds[i][0];
+    if (fds[i][1] > m) {
+      m = fds[i][1];
     }
   }
 
@@ -173,7 +191,7 @@ int main(int argc, char** argv) {
     for (int i = 0; i < number_of_bidders; i++) {
       if (open[i] == 1) {
         std::cout << "set.." << std::endl;
-        FD_SET(fds[i][0], &readset);
+        FD_SET(fds[i][1], &readset);
       }
     }
 
@@ -182,8 +200,8 @@ int main(int argc, char** argv) {
     ii* data;
 
     for (int i = 0; i < number_of_bidders; i++) {
-      if (FD_ISSET(fds[i][0], &readset)) {
-        r = read(fds[i][0], data, sizeof(data));
+      if (FD_ISSET(fds[i][1], &readset)) {
+        r = read(fds[i][1], data, sizeof(data));
         if (r == -1) {
           std::cout << "An error occured while reading from the file descriptor." << std::endl;
           open[i] = 0;
